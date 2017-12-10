63580e3d4973a414ae5d40affddcafb3966c7af5
8bit <lizardscruff@users.noreply.github.com>
2017-12-10 21:30:18 +0900

    Add files via upload

---
+++ startProject2/script.js
@@ -0,0 +1,13 @@
+/*
+https://www.worldwildlife.org/#
+
+- use WWF as an example of what you want to do.
+- It looks like I'm going to have to use jQuery for this
+- Start with the mobile design - possibly just make a new file and tinker with
+  it to start with
+- The WWF site uses a mobile header and after it reaches a certain width
+  it then switches to another header. it's not removed it just switches from
+  one div to the other div. Whoa, mind blown.  
+
+  May be worth a read:
+  http://justuxdesign.com/blog/8-awesome-examples-of-mobile-navigation-on-responsive-websites*/
